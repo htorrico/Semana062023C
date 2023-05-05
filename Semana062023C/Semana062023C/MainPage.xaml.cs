773b79d57c70871e66372af957c91ad77d71e366
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2023-05-05 16:07:04 -0500

    Add project files.

---
+++ Semana062023C/Semana062023C/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana062023C
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
