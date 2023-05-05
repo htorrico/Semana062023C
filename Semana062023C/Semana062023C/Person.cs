dfb4bfffbf19e08c47b4300a1c8aaee47537c1e5
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2023-05-05 16:20:43 -0500

    template class person

---
+++ Semana062023C/Semana062023C/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana062023C
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string DNI { get; set; }
+    }
+}
